dc612d99f5fe9c47766a13304dce90eca70af316
Dhruv071 <127030697+Dhruv071@users.noreply.github.com>
2023-03-05 18:56:02 +0530

    Update Program1.c

--- Program1.c
+++ Program1.c
@@ -3,7 +3,7 @@ int main()
 {
     //Write your code here
     printf("solve this problem in modules...");
-printf("my name is Deepika");
+printf("my name is Dhruv , roll no 2210997071");
     return 0;
 }
 
