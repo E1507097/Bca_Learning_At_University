247c0c361699066cce52072648dd0b5b35c07f62
E1507097 <105140472+E1507097@users.noreply.github.com>
2023-03-13 10:08:13 +0530

    Create Assignment5.c

---
+++ Assignment5.c
@@ -0,0 +1,8 @@
+//Task is to find out passed as well as failed students.
+//Given the score of 'N' students in the class, write a program to compute the maximum score. Use array to store the marks of the students and assume that the maximum number of students
+// in a class is 50. Assume that -1 is used to indicate a student who is absent for the exam. Assume that a student should secure a minimum of 50 marks to pass the exam.
+
+int main()
+{
+  return 0;
+}
