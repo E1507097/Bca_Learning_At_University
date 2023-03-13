8c69626bd04d3dda19a2209cbf5f58b71489be6c
E1507097 <105140472+E1507097@users.noreply.github.com>
2023-03-13 10:03:20 +0530

    Create Assignment3.c

---
+++ Assignment3.c
@@ -0,0 +1,7 @@
+//Can you help your digital design faculty in correcting the assignment on number conversions by writing a program to convert a decimal number to its binary equivalent using a while loop.
+//Example, lets say input is 7, output should be 111
+
+int main()
+{
+  return 0;
+}
