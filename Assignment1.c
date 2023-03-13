e1b66e28c5a89cc3ab688dd9ca9b675c302b1696
E1507097 <105140472+E1507097@users.noreply.github.com>
2023-03-13 09:58:32 +0530

    Create Assignment1.c

---
+++ Assignment1.c
@@ -0,0 +1,7 @@
+//Write a C program that allows the user to enter 'N' numbers and find the total positive numbers and total negative numbers using a for loop.
+
+int main()
+{
+  //add your code here
+  return 0;
+}
