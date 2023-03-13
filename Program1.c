7ad03f9ba8bf2c19d6da85cd2453d6d8246ca48c
E1507097 <105140472+E1507097@users.noreply.github.com>
2023-03-13 09:52:25 +0530

    Update Program1.c

--- Program1.c
+++ Program1.c
@@ -1,10 +1,7 @@
 #include<stdio.h>
 int main() 
 {
-    //Write your code here
-    printf("solve this problem in modules......");
-printf("my name is Deepika");
-    printf("My Name is Shaurya Kajla");
+    
     return 0;
 }
 
