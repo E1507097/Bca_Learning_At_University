8a1f09ad5c850c245b894581545609aee07c30e7
janvi7106 <127727329+janvi7106@users.noreply.github.com>
2023-03-21 13:33:55 +0530

    Update Assignment4.c

--- Assignment4.c
+++ Assignment4.c
@@ -2,6 +2,25 @@
 
 int main()
 {
+  int mat[3][3], i, j, max;
+    printf("Enter any 3*3 matrix: ");
+    for(i=0; i<3; i++)
+    {
+        for(j=0; j<3; j++)
+            scanf("%d", &mat[i][j]);
+    }
+    max = mat[0][0];
+    for(i=0; i<3; i++)
+    {
+        for(j=0; j<3; j++)
+        {
+            if(max<mat[i][j])
+                max = mat[i][j];
+        }
+    }
+    printf("\nLargest Element = %d", max);
+    getch();
+    return 0;
   
-  return 0;
+  
 }
