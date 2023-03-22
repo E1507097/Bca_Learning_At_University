c2d8680ba2766e7faae83fa3c3dab7f590036605
Khushi <127961906+Khushi271@users.noreply.github.com>
2023-03-22 19:43:22 +0530

    Update Program1.c

--- Program1.c
+++ Program1.c
@@ -1,7 +1,10 @@
 #include<stdio.h>
 int main() 
 {
-    
+    printf("solve this problem in modules......");
+    printf("my name is Khushi");
+    printf("my name is Disha");
+    printf("My Name is Kashish");    
     return 0;
 }
 
