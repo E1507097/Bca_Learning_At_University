402439edee3e8c60e3aa18b4fa38f5a8db0be5d7
Khushi <127961906+Khushi271@users.noreply.github.com>
2023-03-22 19:29:10 +0530

    Update Assignment2.c

--- Assignment2.c
+++ Assignment2.c
@@ -2,6 +2,17 @@
 
 int main() 
 {
-  
+   int num,a;
+    a=0;
+    printf("Enter number divisible by 8= ");
+    scanf("%d",&num);
+    while(num%8==0)
+    {
+        printf("\nEnter number divisible by 8=");
+        scanf("%d",&num);
+        a++;
+    }
+    printf("The number of valid numbers entered are %d",a);
+ 
   return 0;
 }
