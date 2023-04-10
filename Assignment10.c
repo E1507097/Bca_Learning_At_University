674ff2d8aec07ab8fe9ab3034c987d0f971e305d
E1507097 <105140472+E1507097@users.noreply.github.com>
2023-04-10 15:41:11 +0530

    Create Assignment10.c

---
+++ Assignment10.c
@@ -0,0 +1,14 @@
+//Write a C code to find the type of array
+//If all elements in the array are odd it is known as "ODD ARRAY"
+//if all elements in the array are even it is known as "EVEN ARRAY"
+//if both even and odd available it is known as MIXED array
+
+
+int findType(int n, int *) //complete this function
+{
+  
+}
+int main()
+{
+  return 0;
+}
