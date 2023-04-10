c6eafad5b1ed49196702692e81e520251386d580
E1507097 <105140472+E1507097@users.noreply.github.com>
2023-04-10 15:47:55 +0530

    Create Assignment13.c

---
+++ Assignment13.c
@@ -0,0 +1,12 @@
+//Design a basic calculator in C. It must be a menu driven program
+//Press 1 for addition
+//Press 2 for substraction
+//Press 3 for multiplication
+//Press 4 for division
+
+//after showing result the main menu should appear again.
+
+int main()
+{
+  return 0;
+}
