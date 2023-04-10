9c2f6e3628bb30b06f0cdc38fef49b4c3a6eb873
E1507097 <105140472+E1507097@users.noreply.github.com>
2023-04-10 15:37:34 +0530

    Create Assignment9.c

---
+++ Assignment9.c
@@ -0,0 +1,12 @@
+//Write a C code to find the frequency of each letter in the name
+//Sample Input: Raman
+//Output: R - 1
+//.       a = 2
+//.       m = 1
+//.       n = 1
+
+int main()
+{
+  
+  return 0;
+}
