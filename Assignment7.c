45174499762612cd0c5556379f95b8b2fb1c0690
janvi7106 <127727329+janvi7106@users.noreply.github.com>
2023-04-27 20:32:28 +0530

    Update Assignment7.c

--- Assignment7.c
+++ Assignment7.c
@@ -2,5 +2,27 @@
 
 int main()
 {
+  char str[100];
+    int len = 0;
+
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    // Find length of string
+    while (str[len] != '\0') {
+        len++;
+    }
+
+    printf("Length of string: %d\n", len);
+
+    // Reverse string
+    char rev_str[100];
+    int i, j;
+    for (i = len - 1, j = 0; i >= 0; i--, j++) {
+        rev_str[j] = str[i];
+    }
+    rev_str[j] = '\0';
+
+    printf("Reverse equivalent of string: %s\n", rev_str);
   return 0;
 }
