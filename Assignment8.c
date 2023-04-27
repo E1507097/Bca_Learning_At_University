33ef05476306d62c912fc19a71e38deecae10666
janvi7106 <127727329+janvi7106@users.noreply.github.com>
2023-04-27 20:33:13 +0530

    Update Assignment8.c

--- Assignment8.c
+++ Assignment8.c
@@ -4,5 +4,27 @@
 
 int main()
 {
+   char str[100];
+    printf("Enter a string: ");
+    scanf("%s", str);
+    selectionSort(str, strlen(str));
+    printf("Sorted string: %s", str);
+    return 0;
+}
+
+void selectionSort(char str[], int n) {
+    int i, j, min_idx;
+    char temp;
+    for (i = 0; i < n-1; i++) {
+        min_idx = i;
+        for (j = i+1; j < n; j++) {
+            if (str[j] < str[min_idx]) {
+                min_idx = j;
+            }
+        }
+        temp = str[i];
+        str[i] = str[min_idx];
+        str[min_idx] = temp;
+    }
   return 0;
 }
