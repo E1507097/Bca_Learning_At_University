29db89858973c22dc4f7ff29bba265fcba53958d
janvi7106 <127727329+janvi7106@users.noreply.github.com>
2023-04-27 20:36:38 +0530

    Update Assignment11.c

--- Assignment11.c
+++ Assignment11.c
@@ -3,6 +3,17 @@
 int main()
 {
   int N = 7;
+  binary = 0, place = 1, remainder;
+
+    while (N> 0) {
+        remainder = N % 2;
+        binary += remainder * place;
+        place *= 10;
+        N /= 2;
+    }
+
+    printf("Binary equivalent: %d", binary);
+
   
   return 0;
 }
